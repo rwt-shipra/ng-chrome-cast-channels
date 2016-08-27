aaac944032992d088c47d3da77da7db178f2e6e3
shipra <rawatshipra15@gmail.com>
2016-08-27 09:01:10 +0530

    no message

--- home/home.controller.js
+++ home/home.controller.js
@@ -538,7 +538,7 @@ var app = angular.module('app')
                 }
                 else {
                     $scope.flashBus = $scope.flashQueue[flashindex];
-                    Speech.speak("Attention Patients! Patient " + flashBus.body.patientName + " please proceed to the doctor " + flashBus.header.doctorName);
+                    Speech.speak("Attention Patients! Patient " + $scope.flashBus.body.patientName + " please proceed to the doctor " + $scope.flashBus.header.doctorName);
                     $timeout(function () {
                         showFlash(++flashindex)
                     }, 6000);
