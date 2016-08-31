539113a733d2d09a7e62eea1dbf6b3a701c620d6
shipra <rawatshipra15@gmail.com>
2016-08-31 19:21:46 +0530

    no message

--- home/home.controller.js
+++ home/home.controller.js
@@ -105,10 +105,8 @@ var app = angular.module('app')
 
             function playDisconnectionSound() {
                 console.log("playing disconnection sound");
-              //  playbreakingnewssound();
-
-                //responsiveVoice.speak("disconnected","Hindi Female", {rate: 1.0});
-                //audio.play("sound")
+                responsiveVoice.speak("disconnected","Hindi Female", {rate: 1.0});
+                audio.play("sound")
             }
 
 
